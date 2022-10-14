97f8e9ac18d443ce0e70d95e03c67525c064bfff
ashmalzahra <ashmal.zahraa@gmail.com>
2022-10-14 18:25:36 +0500

    playRound function

--- file.js
+++ file.js
@@ -10,3 +10,23 @@ function getComputerChoice(){
     }
 }
 
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "Rock" && computerSelection === "Paper"){
+    return ("You lose! Paper beats Rock");}
+    elseif (playerSelection === "Rock" && computerSelection === "Scissor"){
+    return ("You win! Rock beats scissors");}
+    elseif (playerSelection === "Paper" && computerSelection === "Rock" ){
+    return ("You win! Paper beats Rock"); }
+    elseif (playerSelection === "Paper" && computerSelection === "Scissor"){
+    return ("You lose! Scissor beats Paper");}
+    
+    elseif(playerSelection === "Scissor" && computerSelection === "Paper"){
+    return ("You win! Scissor beats Paper");
+    }
+    elseif (playerSelection === "Scissor" && computerSelection === "Rock"){
+    return ("You lose! Rock beats Scissor");
+    }
+    }
+    
+
