b13cca155669371f9fa0e7d53815eb3572094db7
ashmalzahra <ashmal.zahraa@gmail.com>
2022-10-16 21:02:55 +0500

    made parameter playerSelection case insensitive

--- file.js
+++ file.js
@@ -2,40 +2,63 @@ function getComputerChoice(){
     const num= Math.floor(Math.random()*3)
     switch (num) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2: 
-            return "Scissor";
+            return "scissor";
     }
 }
 
-function caseSensitive (playerSelection){
-    return (playerSelection = playerSelection.AtChar()[0].toUpperCase() + playerSelection.slice(1).toLowerCase());
-}
 
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
+    const lowerCasePlayerSelection = playerSelection.toLowerCase();
+    if (lowerCasePlayerSelection === computerSelection){
         return "This is a tie!";
     }
-    if (playerSelection === "Rock") {
-        if (computerSelection === "Paper"){
+    if (lowerCasePlayerSelection === "rock") {
+        if (computerSelection === "paper"){
             return "You lose! Paper beats Rock";
         }
         else return "You win! Rock beats Scissor";
     }
-    if (playerSelection === "Paper"){
-        if (computerSelection === "Scissor"){
+    if (lowerCasePlayerSelection === "paper"){
+        if (computerSelection === "scissor"){
             return "You lose! Scissor beats Paper";
         }
         else return "You win! Paper beats Rock";
     }
-    if (playerSelection === "Scissor"){
-        if (computerSelection === "Rock"){
+    if (lowerCasePlayerSelection === "scissor"){
+        if (computerSelection === "rock"){
             return "You lose! Rock beats Scissor";
         }
         else return "You win! Scissor beats Paper";
     }
+
 }
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+console.log(playRound(playerSelection, computerSelection));
+
+        
+function game() {
+    for (let i= 1; i <= 5; i++){
+        playRound();
+        let playerSelection = prompt(); 
+        if (  result === "You win" ){
+            playerScore ++;
+            console.log(playerScore);
+        }
+        else if (result === "You lost"){
+            compScore ++;
+            console.log(compScore);
+        }
+        ;
+    }
+}
+
+
 
